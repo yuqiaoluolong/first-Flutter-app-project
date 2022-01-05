8b308ff95382b4feae3c9de0862609f23f1fff8f
Harry Liu <2685293507@qq.com>
2022-01-05 15:27:54 +0800

    version 3.0
    achieve the function of generating an infinite word pair list.

--- main.dart
+++ main.dart
@@ -1,25 +1,15 @@
-// ignore_for_file: use_key_in_widget_constructors
-
+// ignore_for_file: prefer_const_constructors, unnecessary_new
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(new MyApp());			// void main() =>
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final WordPair wordPair = new WordPair.random();  // 删掉本行
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          //child: new Text(wordPair.asPascalCase), // 修改本行内容 
-          child: new RandomWords(),                 // 修改成本行代码
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: new RandomWords(),
     );
   }
 }
@@ -30,9 +20,39 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  @override                                  // 新增代码片段 - 开始 ... 
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0); 
+  @override
   Widget build(BuildContext context) {
-    final WordPair wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
-  }                                          // ... 新增的代码片段 - 结束
+    return new Scaffold (                 
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return new Divider();
+        }
+        final int index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
 }
