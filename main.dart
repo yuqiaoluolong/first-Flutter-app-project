ca90190dc4b0279358522bcda1ba2b90f0bf71e0
Harry Liu <2685293507@qq.com>
2022-01-05 15:31:23 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,57 +1,21 @@
-// ignore_for_file: prefer_const_constructors, unnecessary_new
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';			// import
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new MyApp());			// void main() =>
 
-class MyApp extends StatelessWidget {
-  @override
+class MyApp extends StatelessWidget {			
+  @override					//class MyApp, override, Widget build
   Widget build(BuildContext context) {
+    final WordPair wordPair = new WordPair.random();
     return new MaterialApp(
-      title: 'Startup Name Generator',
-      home: new RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => new RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0); 
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold (                 
-      appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (BuildContext _context, int i) {
-        if (i.isOdd) {
-          return new Divider();
-        }
-        final int index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      }
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          child: new Text(wordPair.asPascalCase),
+        ),
       ),
     );
   }
